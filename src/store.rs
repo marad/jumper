cde8a272b5365e1916c0407099ac007db855fa70
Marcin Radoszewski <marcin.radoszewski@allegro.pl>
2023-11-01 10:50:34 +0100

    Reformat store.rs

--- src/store.rs
+++ src/store.rs
@@ -1,5 +1,5 @@
-use sqlx::{SqlitePool, Pool, Sqlite, FromRow, migrate::MigrateDatabase};
 use crate::Res;
+use sqlx::{migrate::MigrateDatabase, FromRow, Pool, Sqlite, SqlitePool};
 
 #[derive(Clone, FromRow, Debug)]
 pub struct SavedPath {
@@ -7,25 +7,26 @@ pub struct SavedPath {
     pub path: String,
 }
 
-
 #[derive(Debug)]
 pub struct Store {
-    pool: Pool<Sqlite>
+    pool: Pool<Sqlite>,
 }
 
 impl Store {
-
     pub async fn create(url: &str) -> Res<Store> {
         if !Sqlite::database_exists(url).await.unwrap_or(false) {
             Sqlite::create_database(url).await?;
         }
         let pool = SqlitePool::connect(url).await?;
 
-        sqlx::query("CREATE TABLE IF NOT EXISTS paths (
+        sqlx::query(
+            "CREATE TABLE IF NOT EXISTS paths (
                 id INTEGER PRIMARY KEY NOT NULL,
                 name VARCHAR(50) UNIQUE NOT NULL,
-                path VARCHAR(500))")
-            .execute(&pool).await?;
+                path VARCHAR(500))",
+        )
+        .execute(&pool)
+        .await?;
         return Ok(Store { pool });
     }
 
@@ -45,7 +46,6 @@ impl Store {
             .await?;
         Ok(path.path)
     }
-
 
     pub async fn list(&mut self) -> Res<Vec<SavedPath>> {
         let data = sqlx::query_as::<_, SavedPath>("SELECT name, path FROM paths")
